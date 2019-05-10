63d4a6f25e4b93c53e5d649e2de00a6139a6e54e
edgar <e.torrielli@gmail.com>
2019-05-10 11:53:52 -0300

    REST saludo de ejemplo: http://localhost:8080/saludo

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # academia-backend
 Proyecto backend de academia cuyo objetivo es proveer servicios REST con las funcionalidades requeridas.
 
-## Tecnologias utilizadas:
+### Tecnologias utilizadas:
 - JDK11
 - Spring Boot 2.2.0
+
+### Generacion del proyecto base:
+-  https://start.spring.io/
---
+++ src/main/java/com/undec/academia/controller/SaludoController.java
@@ -0,0 +1,43 @@
+package com.undec.academia.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author etorrielli
+ */
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/saludo")
+public class SaludoController {
+
+    @GetMapping()
+    public String list() {
+        return "Hola Mundo!";
+    }
+
+    @GetMapping("/{id}")
+    public Object get(@PathVariable String id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Object input) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        return null;
+    }
+
+}
